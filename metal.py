90899da48f09fb0634f60c2229b43a9232bdc122
tp86 <tom0palka+tp86@gmail.com>
2021-08-28 12:16:04 +0000

    Signals

--- metal.py
+++ metal.py
@@ -29,3 +29,30 @@ class Pin(Subject):
     def connect(self, source):
         self.disconnect()
         self._connection = source.subscribe(self)
+
+
+class _SignalMeta(type):
+    def __repr__(cls):
+        return cls.repr
+    __str__ = __repr__
+
+    def __bool__(cls):
+        return cls.value
+
+
+class _Signal(metaclass=_SignalMeta):
+    pass
+
+
+class H(_Signal):
+    repr = '\u0393'
+    value = True
+
+
+class L(_Signal):
+    repr = 'L'
+    value = False
+    flip = H
+
+
+H.flip = L
