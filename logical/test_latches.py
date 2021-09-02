65e14b08de2a6e81b3ed5a24f86212da4496c2ed
tp86 <tom0palka+tp86@gmail.com>
2021-09-02 13:42:42 +0000

    Test SREnable latch

--- logical/test_latches.py
+++ logical/test_latches.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from test_utils import Probe
 
-from logical.latches import SR
+from logical.latches import SR, SREnable
 from physical.signals import H, L
 
 
@@ -48,3 +48,63 @@ class SRTest(TestCase):
         self.assertEqual([L, H, L], self.probe_q.results)
         self.assertEqual([H, L, H], self.probe_q_.results)
         self.assertEqual([H, L], probe.results)
+
+
+class SREnableTest(TestCase):
+
+    def setUp(self):
+        self.probe_q = Probe()
+        self.probe_q_ = Probe()
+        self.sre = SREnable()
+
+    def test_srenable_initializes_in_reset_state(self):
+        self.sre.Q.subscribe(self.probe_q)
+        self.sre.Q_.subscribe(self.probe_q_)
+
+        self.assertEqual([L], self.probe_q.results)
+        self.assertEqual([H], self.probe_q_.results)
+
+    def test_srenable_does_not_react_without_enable_high(self):
+        self.sre.Q.subscribe(self.probe_q)
+        self.sre.Q_.subscribe(self.probe_q_)
+
+        self.sre.S.on_next(H)
+        self.sre.S.on_next(L)
+        self.assertEqual([L], self.probe_q.results)
+        self.assertEqual([H], self.probe_q_.results)
+
+        self.sre.R.on_next(L)
+        self.sre.R.on_next(H)
+        self.assertEqual([L], self.probe_q.results)
+        self.assertEqual([H], self.probe_q_.results)
+
+    def test_srenable_sets_and_resets_on_enable_high(self):
+        self.sre.Q.subscribe(self.probe_q)
+        self.sre.Q_.subscribe(self.probe_q_)
+
+        self.sre.S.on_next(H)
+        self.assertEqual([L], self.probe_q.results)
+        self.assertEqual([H], self.probe_q_.results)
+        self.sre.EN.on_next(H)
+        self.assertEqual([L, H], self.probe_q.results)
+        self.assertEqual([H, L], self.probe_q_.results)
+        self.sre.EN.on_next(L)
+        self.sre.S.on_next(L)
+
+        self.sre.R.on_next(H)
+        self.assertEqual([L, H], self.probe_q.results)
+        self.assertEqual([H, L], self.probe_q_.results)
+        self.sre.EN.on_next(H)
+        self.assertEqual([L, H, L], self.probe_q.results)
+        self.assertEqual([H, L, H], self.probe_q_.results)
+        self.sre.EN.on_next(L)
+        self.sre.R.on_next(L)
+
+        self.sre.EN.on_next(H)
+        self.sre.S.on_next(H)
+        self.assertEqual([L, H, L, H], self.probe_q.results)
+        self.assertEqual([H, L, H, L], self.probe_q_.results)
+        self.sre.S.on_next(L)
+        self.sre.R.on_next(H)
+        self.assertEqual([L, H, L, H, L], self.probe_q.results)
+        self.assertEqual([H, L, H, L, H], self.probe_q_.results)
