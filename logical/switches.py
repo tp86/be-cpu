ecea1858bdc012390f939c8533bec4c4380aa41a
tp86 <tom0palka+tp86@gmail.com>
2021-09-22 08:07:08 +0000

    Remove transceiver - not needed in current approach

--- logical/switches.py
+++ logical/switches.py
@@ -1,7 +1,6 @@
 from rx.operators import filter as rxfilter, starmap
 
 from physical.elements import Pin
-from .gates import And, Not
 
 
 class Buffer:
@@ -16,29 +15,3 @@ class Buffer:
                 starmap(lambda _, s: s)
             )
         )
-
-
-class _BusTranceiver_1bit:
-    def __init__(self) -> None:
-        a_buf = Buffer()
-        b_buf = Buffer()
-
-        self.A = b_buf.OUT
-        self.B = a_buf.OUT
-        a_buf.IN.connect(b_buf.OUT)
-        b_buf.IN.connect(a_buf.OUT)
-
-        a_and = And()
-        b_and = And()
-        a_buf.ENABLE.connect(a_and.C)
-        b_buf.ENABLE.connect(b_and.C)
-        self.DIR = a_and.B
-        en_a_not = Not()
-        en_b_not = Not()
-        dir_b_not = Not()
-        b_and.B.connect(dir_b_not.B)
-        b_and.A.connect(en_b_not.B)
-        a_and.A.connect(en_a_not.B)
-        dir_b_not.A.connect(self.DIR)
-        self.EN_ = en_a_not.A
-        en_b_not.A.connect(self.EN_)
